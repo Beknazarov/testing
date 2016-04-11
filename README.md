7042b55bab0e65ee5c0f71c5af6e752338ed2769
kgstan <chingizkg@gmail.com>
2016-04-11 13:58:14 +0600

    Zagotovka

---
+++ README.md
@@ -0,0 +1,10 @@
+Testing
+=========
+
+## How to install
+
+1 pip install -r requirements.txt 
+
+2 Change(rename) file from 'testing_kg/testing_kg/settings/samplelocal.py' TO 'local.py'
+
+3 We are using MySQL (You can download from  [Download link](https://dev.mysql.com/downloads/mysql/))
---
+++ testing/testing/urls.py
@@ -0,0 +1,26 @@
+""" Default urlconf for testingKg """
+
+from django.conf.urls import include, patterns, url
+from django.conf import settings
+from django.contrib import admin
+from django.conf.urls.static import static
+
+admin.autodiscover()
+
+
+urlpatterns = [
+    url(r'', include('base.urls')),
+    # url(r'^accounts/', include('accounts.urls')),
+    # url(r'^profiles/', include('profiles.urls', namespace='profile')),
+    url(r'^admin/', include(admin.site.urls)),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +\
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
